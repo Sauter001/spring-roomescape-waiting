09f61e001aa233a2f33fc004bb8b0d71550e91b7
sauter001 <lsj457@naver.com>
2026-06-11 14:03:17 +0900

    chore: add deploy script for spring-roomescape-waiting project

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+SERVER_HOME="$HOME/spring-roomescape-waiting"
+PID_FILE="$SERVER_HOME/app.pid"
+
+if [ ! -d "$SERVER_HOME" ]; then
+  git clone https://github.com/Sauter001/spring-roomescape-waiting "$SERVER_HOME"
+fi
+cd "$SERVER_HOME" || exit
+git checkout sauter001
+git pull origin sauter001
+
+./gradlew clean
+./gradlew bootJar
+
+cd ./build/libs || exit
+if [ -f "$PID_FILE" ]; then
+  kill "$(cat "$PID_FILE")" 2>/dev/null
+fi
+nohup java -jar spring-roomescape-waiting-0.0.1-SNAPSHOT.jar &
+echo $! > "$SERVER_HOME"/app.pid
