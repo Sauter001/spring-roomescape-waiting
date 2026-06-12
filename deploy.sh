88f8e14e3eb807663c1566b52cd0a6bad0f8e58c
sauter001 <lsj457@naver.com>
2026-06-12 09:55:48 +0900

    chore: fix redundant log redirection in deploy script

--- deploy.sh
+++ deploy.sh
@@ -14,10 +14,9 @@ git pull origin sauter001
 ./gradlew clean
 ./gradlew bootJar
 
-cd ./build/libs || exit
 if [ -f "$PID_FILE" ]; then
   kill "$(cat "$PID_FILE")" 2>/dev/null
 fi
 cd "$HOME" || exit
-nohup java -jar "$SERVER_HOME"/build/libs/spring-roomescape-waiting-0.0.1-SNAPSHOT.jar > output.log > /dev/null 2>&1 &
+nohup java -jar "$SERVER_HOME"/build/libs/spring-roomescape-waiting-0.0.1-SNAPSHOT.jar > output.log /dev/null 2>&1 &
 echo $! > "$SERVER_HOME"/app.pid
